f84475a5bd10f4a6399ce922426073929d111adb
Rebira Adugna <rebikman9@gmail.com>
2026-09-25 14:01:21 +0300

    Load MongoDB URI and JWT secret from environment

--- clean_Architecture/task_api/Delivery/main.go
+++ clean_Architecture/task_api/Delivery/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"task-api/Delivery/controllers"
@@ -17,62 +18,42 @@ import (
 
 func ConnectMongoDB(uri string) (*mongo.Client, error) {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-
-	opts := options.Client().
-		ApplyURI(uri).
-		SetServerAPIOptions(serverAPI)
-
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
 	client, err := mongo.Connect(opts)
-	if err != nil {
-		return nil, err
-	}
-
+	if err != nil { return nil, err }
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-
-	if err := client.Ping(ctx, nil); err != nil {
-		return nil, err
-	}
-
+	if err := client.Ping(ctx, nil); err != nil { return nil, err }
 	return client, nil
 }
 
-func main() {
-	mongoURI := "mongodb://localhost:27017"
+func requiredEnv(name string) string {
+	value := os.Getenv(name)
+	if value == "" { log.Fatalf("%s is required", name) }
+	return value
+}
 
+func main() {
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" { mongoURI = "mongodb://localhost:27017" }
 	client, err := ConnectMongoDB(mongoURI)
-	if err != nil {
-		log.Fatal(err)
-	}
+	if err != nil { log.Fatal(err) }
 
 	db := client.Database("task_api")
 	taskCollection := db.Collection("tasks")
 	userCollection := db.Collection("users")
 
-	// Infrastructure
-	jwtSecret := "my-secret-key"
+	jwtSecret := requiredEnv("JWT_SECRET")
 	jwtService := infrastructure.NewJwtService(jwtSecret, "task-api")
 	passwordService := infrastructure.NewPasswordService()
-
-	// Repositories
 	taskRepo := repositories.NewTaskRepository(taskCollection)
 	userRepo := repositories.NewUserRepository(userCollection)
-
-	// Usecases
 	taskUsecase := usecases.NewTaskUsecase(taskRepo)
 	userUsecase := usecases.NewUserUsecase(userRepo, passwordService, jwtService)
-
-	// Controllers
 	controller := controllers.NewController(taskUsecase, userUsecase)
-
-	// Middlewares
 	authMiddleware := infrastructure.AuthMiddleware(jwtService)
-
-	// Routers
 	r := routers.SetupRouter(controller, authMiddleware)
 
 	log.Println("Server running on :8080")
-	if err := r.Run(":8080"); err != nil {
-		log.Fatal(err)
-	}
+	if err := r.Run(":8080"); err != nil { log.Fatal(err) }
 }
